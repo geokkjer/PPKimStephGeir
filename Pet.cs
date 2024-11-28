5ef2d2d18c4875d13ed1918b5116ce6fd1595632
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-11-28 12:41:53 +0100

    Kim

--- Pet.cs
+++ Pet.cs
@@ -15,20 +15,99 @@ namespace MasterSpace
             Name = name;
             Type = type;
             Age = age;
-            Cuddle = true;
+            Cuddle = false;
             Hungry = false;
             NeedToilet = false;
 
         }
+        
         public void InteractMenu()
         {
-            Console.WriteLine(" Hey " + Name + "!");
-            Console.WriteLine(" Type: " + Type);
-            Console.WriteLine(" Age " + Age);
-            Console.WriteLine((Cuddle) ? " Need to cuddle " : " Has been cuddled ");
-            Console.WriteLine((Hungry) ? " Is starving " : " Has a full belly");
-            Console.WriteLine((NeedToilet) ? " Need to poop " : " Doesn't need to to poop");
-            Console.WriteLine("\nwhat do you want to do?");
+            bool running = true;
+            
+            while (running)
+            {
+                Thread.Sleep(1000);
+               Console.Clear();
+
+                Console.WriteLine(" Hey " + Name + "!");
+                Console.WriteLine(" Type: " + Type);
+                Console.WriteLine(" Age " + Age);
+                Console.WriteLine((Cuddle) ?  " Need to cuddle " : " Has been cuddled ");
+                Console.WriteLine((Hungry) ?  " Is starving " : " Has a full belly");
+                Console.WriteLine((NeedToilet) ?   " Need to poop " : " Doesn't need to to poop");
+
+
+                Console.WriteLine("\nwhat do you want to do?");
+                Console.WriteLine();
+                Console.WriteLine("1. Cuddle");
+                Console.WriteLine("2. Feed");
+                Console.WriteLine("3. WC");
+                Console.WriteLine("4. Exit");
+                var userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Cuddles();
+                        break;
+                    case "2":
+                        Feed();
+                        break;
+                    case "3":
+                        NeedToilet = true;
+                        Console.WriteLine("Buisness Done!");
+                        break;
+                    case "4":
+                        running = false;
+                        break;
+
+                }
+
+
+
+            }
+        }
+
+        void Cuddles()
+        {
+            if (Cuddle)
+            {
+                Console.WriteLine("Happy Camper!");
+                Cuddle = false;
+            }
+            else
+            {
+                Console.WriteLine("Bites!!");
+                Cuddle = true;
+            }
+        }
+
+        void Feed()
+        {
+            if (Hungry)
+            {
+                Console.WriteLine("Fed And Well");
+                Hungry = false;
+            }
+            else
+            {
+                Console.WriteLine("Throws up!!");
+                Hungry = true;
+            }
+        }
+
+        void Walk()
+        {
+            if (NeedToilet)
+            {
+                Console.WriteLine("Jumps all around and Plays");
+                NeedToilet = false;
+            }
+            else
+            {
+                Console.WriteLine("Run Away!!");
+                NeedToilet = true;
+            }
         }
 
     }
